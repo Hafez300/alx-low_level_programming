83eb8fd3743775d1b46608a953c12e808846cd08
ziad <ziadwhafez@gmail.com>
2023-08-10 18:29:10 -0700

    name

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,16 +3,13 @@
 /**
  * main - Entry point
  * 
- * Description: A C program that prints with printf function
+ * Description: A C program that prints with printf function.
  * 
- * Return: always 0 (Success)
+ * Return: Always 0 (success)
 */
 
-int main (void)
+int main(void)
 {
-	
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
-
