3e576a592e101cb19f78eb7f4a7550cf201a6b2d
ziad <ziadwhafez@gmail.com>
2023-08-13 04:25:37 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - entry pointer
+ * main - Entry point
  *
- * Description: print the value of n status:
- * 		greater than, is zero and is not less than 6
- *
- * Return: 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
